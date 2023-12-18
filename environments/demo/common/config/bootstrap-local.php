b1fefffa86ca849a6b2ae428d35f5b7213ba7a37
abdallah jaber <abdallah.j@codendot.com>
2023-12-18 22:47:21 +0200

    ...

--- environments/demo/common/config/bootstrap-local.php
+++ environments/demo/common/config/bootstrap-local.php
@@ -3,3 +3,4 @@ Yii::setAlias('@adminWeb', 'https://phpstack-1184945-4163793.cloudwaysapps.com/a
 Yii::setAlias('@staticWeb', 'https://phpstack-1184945-4163793.cloudwaysapps.com/static');
 Yii::setAlias('@apiWeb', 'https://phpstack-1184945-4163793.cloudwaysapps.com/api/web');
 Yii::setAlias('@technicianWeb', 'https://phpstack-1184945-4163793.cloudwaysapps.com/technician/web');
+Yii::setAlias('@vendorWeb', 'https://phpstack-1184945-4163793.cloudwaysapps.com/vendor');
